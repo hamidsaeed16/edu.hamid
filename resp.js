195ddcc2ea6e62ce54390ddbfb87ffc3599e42e1
hamidsaeed16 <hamidsaeedid84@gmail.com>
2025-02-08 19:20:22 +0500

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
+rightnav = document.querySelector('.rightnav')
+
+
+burger.AddEventListener('click', () => {
+    rightNav.classlist.toggle(v - classresp);
+    navlist.classlist.toggle(v - classresp);
+    navbar.classlist.toggle(h - nav - resp);
+
+})
